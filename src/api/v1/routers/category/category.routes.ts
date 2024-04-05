679e641f6acc0815a22502fb748c921f93709bf4
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-04-06 00:10:29 +0530

    fix:category api fixed

--- src/api/v1/routers/category/category.routes.ts
+++ src/api/v1/routers/category/category.routes.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { createCategory } from "../../controllers/category/category.controller";
+import {
+	createCategory,
+	deleteCategory,
+	editCategory,
+	getCategories
+} from "../../controllers/category/category.controller";
 import multer from "multer";
 const router = express.Router();
 
@@ -7,5 +12,11 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post("/createCategory", upload.fields([{ name: "images", maxCount: 10 }]), createCategory);
+
+router.patch("/editCategory/:_id", upload.fields([{ name: "images", maxCount: 10 }]), editCategory);
+
+router.route("/getCategory").get(getCategories);
+
+router.route("/deleteCategory/:_id").delete(deleteCategory);
 
 module.exports = router;
