e0a249f5edc2a41b4137792b0d8e03b7b04eb8a2
anik0810 <anikdutta0810@gmail.com>
2024-04-08 19:44:30 +0530

    FIX : notification date

--- src/models/shcemaDefinations/notification.schema.ts
+++ src/models/shcemaDefinations/notification.schema.ts
@@ -1,10 +1,14 @@
 import { Schema, VirtualTypeOptions } from "mongoose";
 import SCHEMA_DEFINITION_PROPERTY from "../../constants/model/model.constant";
 import { INotification } from "../../ts/interfaces/notification.interface";
+import { GENERAL_SCHEMA_OPTIONS } from "../../constants/model/schemaOption";
 
-const notificationSchema: Schema<INotification> = new Schema<INotification>({
-    user_object_id:SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
-    notification_title:SCHEMA_DEFINITION_PROPERTY.requiredString
-});
+const notificationSchema: Schema<INotification> = new Schema<INotification>(
+	{
+		user_object_id: SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
+		notification_title: SCHEMA_DEFINITION_PROPERTY.requiredString
+	},
+	GENERAL_SCHEMA_OPTIONS
+);
 
 export default notificationSchema;
--- src/models/shcemaDefinations/rating.schema.ts
+++ src/models/shcemaDefinations/rating.schema.ts
@@ -2,12 +2,16 @@ import { Schema, VirtualTypeOptions } from "mongoose";
 import SCHEMA_DEFINITION_PROPERTY from "../../constants/model/model.constant";
 import { IRatingSchema } from "../../ts/interfaces/rating.interface";
 import UserModel from "../user.model";
+import { GENERAL_SCHEMA_OPTIONS } from "../../constants/model/schemaOption";
 
-const ratingSchema: Schema<IRatingSchema> = new Schema<IRatingSchema>({
-	business_object_id: SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
-	user_object_id: SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
-	rating: SCHEMA_DEFINITION_PROPERTY.optionalNullNumber
-});
+const ratingSchema: Schema<IRatingSchema> = new Schema<IRatingSchema>(
+	{
+		business_object_id: SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
+		user_object_id: SCHEMA_DEFINITION_PROPERTY.requiredObjectId,
+		rating: SCHEMA_DEFINITION_PROPERTY.optionalNullNumber
+	},
+	{ ...GENERAL_SCHEMA_OPTIONS, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 const userVirtualReference: VirtualTypeOptions<IRatingSchema> = {
 	ref: UserModel,
